0926d20a3d89c5294b1e3b87911af23f902a6f6f
Rafael Portácio <rafaelportacio19@gmail.com>
2023-04-23 12:53:25 -0300

    Fix colisao

--- pipeline_full.cpp
+++ pipeline_full.cpp
@@ -115,7 +115,7 @@ void calcula_carros(json * hash_agg, std::vector<std::vector<int>> * frames_inde
 	
 	// número de frames no qual consideraremos como risco de colisão
 	// caso seja previsto que os carros batam após esse número de frames
-	int frame_tolerancia_colisao = 1;
+	int frame_tolerancia_colisao = 100;
 
     while (true){
 
@@ -212,15 +212,18 @@ void calcula_carros(json * hash_agg, std::vector<std::vector<int>> * frames_inde
                     if (carro.key() == carro2.key()){ continue; }
                     //checa se após o dado número de frames um carro ultrapassará o outro (assim, colidindo)
                     if( carro.value()[0] == carro2.value()[0]){
-							double pos = carro.value()[0];
-							double pos2 = carro2.value()[0];
+							double pos = carro.value()[1];
+							double pos2 = carro2.value()[1];
 							std::string placa = carro.key();
 							double pos_prevista = rodovia_frame_agg[rodovia][frame][placa]["Posição Prevista"];
 							std::string placa2 = carro2.key();
 							double pos_prevista2 = rodovia_frame_agg[rodovia][frame][placa2]["Posição Prevista"];
 							if ((pos > pos2 and pos_prevista < pos_prevista2)
-							or (pos < pos2 and pos_prevista > pos_prevista2)){
-								(rodovia_frame_agg[rodovia][frame][placa]["Risco Colisão"]).push_back(placa2);								
+							or (pos < pos2 and pos_prevista > pos_prevista2)
+							or (std::abs(pos-pos2) <= (*parametros)[rodovia]["tamanhoCarro"])
+							or (std::abs(pos_prevista-pos_prevista2) <= (*parametros)[rodovia]["tamanhoCarro"])){
+								std::cout << frame << " " << placa << std::endl;
+								(rodovia_frame_agg[rodovia][frame][placa]["Risco Colisão"]).push_back(placa2);							
 						}
                     }
 
