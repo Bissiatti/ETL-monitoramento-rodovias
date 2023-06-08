7428e0def9da345a9d3f02c7ea0231ec01d4501e
victorbombarda <b41311@fgv.edu.br>
2023-06-08 15:13:54 -0300

    Mais mudancas rs

--- spark_version/notebooks/mock/worker.py
+++ spark_version/notebooks/mock/worker.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery('tasks', broker='pyamqp://guest@localhost:5672//')
+app = Celery('tasks', broker='pyamqp://guest@host.docker.internal:5672//')
 
 if __name__ == '__main__':
     app.worker_main(['worker', '--loglevel=INFO'])
