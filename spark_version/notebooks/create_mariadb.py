c7424ee373fc4c8185c99138d5f308de18d994d7
Victor <70376108+victorbombarda@users.noreply.github.com>
2023-06-20 14:25:22 -0300

    Update create_mariadb.py

--- spark_version/notebooks/create_mariadb.py
+++ spark_version/notebooks/create_mariadb.py
@@ -1,6 +1,8 @@
 import mysql.connector as database
 import os
 
+print("Establishing connection to the Database")
+
 connection = database.connect(
     host="host.docker.internal",
     port=3306,
@@ -11,6 +13,11 @@ connection = database.connect(
 
 cursor = connection.cursor()
 
-
+file = open('create_final_db.sql', 'r')
+query = file.read()
 
 cursor.execute(query)
+print("Schema and Tables succesfully created")
+
+cursor.close()
+connection.close()
