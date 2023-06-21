0645469018d68c3f1ba226d83c148fab45cff6dd
victorbombarda <b41311@fgv.edu.br>
2023-06-21 10:38:06 -0300

    Consertado docker compose

--- spark_version/notebooks/.ipynb_checkpoints/create_final_db-checkpoint.sql
+++ spark_version/notebooks/.ipynb_checkpoints/create_final_db-checkpoint.sql
@@ -9,6 +9,8 @@ USE dashboard;
 
 CREATE TABLE carros (
     placa varchar(10)  NOT NULL,
+    horario_registro bigint  NOT NULL,
+    rodovia varchar(20) NOT NULL,
     pos_x float(6,3)  NOT NULL,
     pos_y float(6,3)  NOT NULL,
     aceleracao float(6,3)  NULL,
@@ -16,18 +18,19 @@ CREATE TABLE carros (
     multas int  NULL,
     risco_colisao bool  NULL,
     direcao_perigosa bool  NULL,
-    velocidade_acima bool  NULL,
-    CONSTRAINT carros_pk PRIMARY KEY (placa)
+    CONSTRAINT carros_pk PRIMARY KEY (horario_registro, placa)
+
+    -- velocidade_acima bool  NULL,
 );
 
 -- Table: contem
-CREATE TABLE contem (
-    id int  NOT NULL AUTO_INCREMENT,
-    carros_placa varchar(10)  NOT NULL,
-    rodovias_horario_registro bigint  NOT NULL,
-    rodovias_nome_rodovia varchar(20)  NOT NULL,
-    CONSTRAINT contem_pk PRIMARY KEY (id)
-);
+-- CREATE TABLE contem (
+--     id int  NOT NULL AUTO_INCREMENT,
+--     carros_placa varchar(10)  NOT NULL,
+--     rodovias_horario_registro bigint  NOT NULL,
+--     rodovias_nome_rodovia varchar(20)  NOT NULL,
+--     CONSTRAINT contem_pk PRIMARY KEY (id)
+-- );
 
 -- Table: rodovias
 CREATE TABLE rodovias (
@@ -52,4 +55,3 @@ ALTER TABLE contem ADD CONSTRAINT contem_rodovias FOREIGN KEY contem_rodovias (r
     REFERENCES rodovias (horario_registro,nome_rodovia);
 
 -- End of file.
-
