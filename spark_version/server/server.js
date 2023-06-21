164fc3bb408f57d07ef6772a90a2d363bdc85dfc
Bissiatti <emanulebissiatti@outlook.com.br>
2023-06-21 11:43:34 -0300

    bd on nodejs

--- spark_version/server/server.js
+++ spark_version/server/server.js
@@ -7,33 +7,68 @@ const port = 3000; // porta do servidor
 app.use(express.static('public'));
 
 const mariadb = require('mariadb');
+
 const pool = mariadb.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '8pJcpe/h0h_%',
-  port: 3306
+    // host: "host.docker.internal",
+    host: "localhost",
+    port: 3306,
+    user: 'root',
+    password: 'secret',
+    database: "dashboard",
+    acquireTimeout: 20000 // 20 seconds
 });
 
+// let conn = await pool.getConnection();
+
 async function getData() {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    const rows = await conn.query("SELECT * ");
-    console.log(rows); // [ {val: 1}, meta: ... ]
-  } catch (err) {
-    throw err;
-  } finally {
-    if (conn) return conn.end();
-  }
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const rows = await conn.query("SELECT COUNT(*) FROM carros;");
+        console.log(rows);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) return conn.end();
+    }
 }
 
 
+// async function getData() {
+//   let conn;
+//   try {
+//     conn = await pool.getConnection();
+//     const use = await conn.query("USE dashboard");
+//     console.log(use);
+//     const rows = await conn.query("SELECT * from dashboard.carros limit 1");
+//     console.log(rows); // [ {val: 1}, meta: ... ]
+//   } catch (err) {
+//     throw err;
+//   } finally {
+//     if (conn) return conn.end();
+//   }
+//  }
+
+getData().then((data) => {
+  console.log(data)
+});
+
 app.get('/data', (req, res) => {
   getData().then((data) => {
+    console.log(data);
     res.send(data);
-  })
+  });
+  // getData().then((data) => {
+  //   res.send(data);
+  // })
+});
+
+app.get('/test', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
+
