f9e5a1c63ee1577ad5626adfc4c3b24a3653faaf
Bissiatti <emanulebissiatti@outlook.com.br>
2023-06-21 15:04:43 -0300

    final front

--- spark_version/server/public/script.js
+++ spark_version/server/public/script.js
@@ -269,42 +269,79 @@ fetch('/data')
 
   });
 
-  function updateData() {
-    fetch('/data')
-      .then(response => response.json())
-      .then(data => {
-        // Insert data into HTML
-        const element = document.getElementById('data-container');
-        console.log(data);
-        const table = document.createElement('table');
-        const headerRow = document.createElement('tr');
-  
-        // Create table headers
-        Object.keys(data[0]).forEach(key => {
-          const th = document.createElement('th');
-          th.textContent = key;
-          headerRow.appendChild(th);
-        });
-        table.appendChild(headerRow);
-  
-        // Create table rows
-        data.forEach(rowData => {
-          const tr = document.createElement('tr');
-          Object.values(rowData).forEach(value => {
-            const td = document.createElement('td');
-            td.textContent = value;
-            tr.appendChild(td);
-          });
-          table.appendChild(tr);
-        });
-  
-        // Insert table into HTML
-        element.innerHTML = ''; // Clear previous data
-        element.appendChild(table);
+function updateData() {
+  fetch('/data')
+    .then(response => response.json())
+    .then(data => {
+      // Insert data into HTML
+      const element = document.getElementById('data-container');
+      console.log(data);
+      const table = document.createElement('table');
+      const headerRow = document.createElement('tr');
+
+      // Create table headers
+      Object.keys(data[0]).forEach(key => {
+        const th = document.createElement('th');
+        th.textContent = key;
+        headerRow.appendChild(th);
       });
-  }
+      table.appendChild(headerRow);
+
+      // Create table rows
+      data.forEach(rowData => {
+        const tr = document.createElement('tr');
+        Object.values(rowData).forEach(value => {
+          const td = document.createElement('td');
+          td.textContent = value;
+          tr.appendChild(td);
+        });
+        table.appendChild(tr);
+      });
+
+      // Insert table into HTML
+      element.innerHTML = ''; // Clear previous data
+      element.appendChild(table);
+    });
+}
+
+function updateData2() {
+  fetch('/rodovia')
+    .then(response => response.json())
+    .then(data => {
+      // Insert data into HTML
+      const element = document.getElementById('data-container2');
+      console.log(data);
+      const table = document.createElement('table');
+      const headerRow = document.createElement('tr');
+
+      // Create table headers
+      Object.keys(data[0]).forEach(key => {
+        const th = document.createElement('th');
+        th.textContent = key;
+        headerRow.appendChild(th);
+      });
+      table.appendChild(headerRow);
+
+      // Create table rows
+      data.forEach(rowData => {
+        const tr = document.createElement('tr');
+        Object.values(rowData).forEach(value => {
+          const td = document.createElement('td');
+          td.textContent = value;
+          tr.appendChild(td);
+        });
+        table.appendChild(tr);
+      });
+
+      // Insert table into HTML
+      element.innerHTML = ''; // Clear previous data
+      element.appendChild(table);
+    });
+}
+  
   
   let intervalId;
+
 
   realTime.addEventListener('change', function() {
     if (realTime.checked) {
@@ -323,6 +360,7 @@ fetch('/data')
 if (realTime.checked) {
   console.log('checked')
   setInterval(updateData, 1000); // Call updateData every 1000ms (1 second)
+  setInterval(updateData2, 1000); // Call updateData every 1000ms (1 second)
 }else{
   console.log('not checked')
 }
--- spark_version/server/server.js
+++ spark_version/server/server.js
@@ -59,8 +59,39 @@ function getData() {
   });
 }
 
+function getRodovia() {
+  return new Promise((resolve, reject) => {
+    pool.getConnection()
+      .then(conn => {
+        const str_query = "SELECT * FROM rodovias WHERE horario_registro = (SELECT MAX(horario_registro) FROM carros);"
+        //const str_query = "SELECT * FROM carros WHERE horario_registro = "+i+");"
+        //i = i + 1
+        conn.query(str_query)
+          .then(rows => {
+            resolve(rows);
+            conn.end();
+          })
+          .catch(err => {
+            reject(err);
+            conn.end();
+          });
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
 app.get('/data', (req, res) => {
   getData().then((data) => {
+    res.send(JSON.stringify(data, (key, value) =>
+      typeof value === 'bigint' ? value.toString() : value
+    ));
+  });
+});
+
+app.get('/rodovia', (req, res) => {
+  getRodovia().then((data) => {
     res.send(JSON.stringify(data, (key, value) =>
       typeof value === 'bigint' ? value.toString() : value
     ));
